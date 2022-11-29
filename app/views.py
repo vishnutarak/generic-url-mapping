25d057548d7d8623e2538d4286524c3bda875aa2
vishnutarak <you@example.comvishnutarak123@gmail.com>
2022-11-29 22:51:14 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def A1_FIRST(request):
+    return render(request,'A1_FIRST.html')
+
+def A1_SECOND(request):
+    return render(request,'A1_SECOND.html')
